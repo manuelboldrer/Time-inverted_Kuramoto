4a44f6474a5f13b71ef6aca5d2b1c93d3be5430f
manue <manuel.boldrer@gmail.com>
2024-04-13 00:05:55 +0200

    clean

--- Kuramoto_python/main.py
+++ Kuramoto_python/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import pdist
@@ -27,14 +28,14 @@ def kuramoto(x, K, N, Omega, Ad):
 def fun(X, alpha):
     X = alpha * X
     t = np.mod(X, 2 * np.pi)
-    x = 20 * np.cos(7*t)
+    x = 20 * np.cos(9*t)
     y = 20 * np.sin(5*t)#*np.sin(5*t)
-    z = 7 + 5 * np.sin(1*t)
+    z = 7 + 2 * np.sin(1*t)
     return x,y,z
 
 # Parameters
-N = 12
-p = 4
+N = 14
+p = 5
 Ad = circular_undirected_adjacency_matrix(N) 
 A = 20
 B = 20
@@ -45,8 +46,8 @@ rs = np.sin(np.pi/N)*np.sqrt(A**2+B**2)
 
 # Initializations
 theta = np.zeros((N, iter+1))
-K = 1* np.ones((1, N))
-Omega = 1/N* alpha * np.ones((N,iter+1))
+K = 4* np.ones((1, N))
+Omega = (0.04/ alpha ) * np.ones((N,iter+1))+ np.random.normal(loc=0, scale=0.01, size=(N, iter+1))
 ff = np.zeros((N, iter+1))
 x, y, z = np.zeros((iter+1, N)), np.zeros((iter+1, N)), np.zeros((iter+1, N))
 xnext, ynext, znext = np.zeros((iter+1, N)), np.zeros((iter+1, N)), np.zeros((iter+1, N))
@@ -55,8 +56,8 @@ xnext, ynext, znext = np.zeros((iter+1, N)), np.zeros((iter+1, N)), np.zeros((it
 for j in range(N):
     theta[j, 0] = np.mod((j-1) * 2 * np.pi * p / N,2*np.pi) #+ np.random.rand
 
-theta[1,0]= theta[1,0] - 0.05
-theta[9,0]= theta[9,0] + 0.05
+#theta[1,0]= theta[1,0] - 0.05
+#theta[9,0]= theta[9,0] + 0.05
 
 
 # Time integration
@@ -65,7 +66,6 @@ dt = 0.1
 plt.ion()
 fig, ax= plt.subplots()
 ax = fig.add_subplot(111, projection='3d')
-#ax1 = fig.add_subplot(111, projection='3d')
 
 co = 1
 xX, yY, zZ = [], [], []
@@ -81,6 +81,12 @@ yb = [-B, -B, B, B, -B]
 # Plot the square
 
 for kk in range(iter):
+    
+    
+    
+    #if kk == 30:
+    #    Omega = (0.0/ alpha ) * np.ones((N,iter+1))+ np.random.normal(loc=0, scale=0.01, size=(N, iter+1))
+
     for i in range(N):
         counte = 0
         for j in range(N):
@@ -90,7 +96,7 @@ for kk in range(iter):
 
     for j in range(N):
         x[kk, j], y[kk, j], z[kk, j] = fun(theta[j, kk], alpha)
-    print(x[kk, :] , y[kk, :], z[kk,:], "theta", theta[:,kk])
+    #print(x[kk, :] , y[kk, :], z[kk,:], "theta", theta[:,kk])
 
     k1 = kuramoto(theta[:, kk], K, N, Omega[:,kk], Ad)
     k2 = kuramoto(theta[:, kk] + 0.5 * dt * k1, K, N, Omega[:,kk], Ad)
@@ -107,19 +113,33 @@ for kk in range(iter):
     ff[:, kk+1] = (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
     for j in range(N):
 
-        xnext[kk, j], ynext[kk, j], znext[kk, j] = fun(theta[j, kk+1], alpha)
+        xnext[kk, j], ynext[kk, j], znext[kk, j] = fun(theta[j, kk+1], alpha) 
 
         vel = np.sqrt((xnext[kk,j]-x[kk,j])**2 +(ynext[kk,j]-y[kk,j])**2 +(ynext[kk,j]-y[kk,j])**2)
         #if j <6:
-        #    circle = plt.Circle([x[kk, j] , y[kk, j]], rs, color='b', fill=False)
+    #circle = plt.Circle([x[kk, j] , y[kk, j]], rs, color='b', fill=False)
         #else:
     #for j in range(0,50):
-    #    circle = plt.Circle([x[kk, j] , y[kk, j]], rs, color='r', fill=False) 
-    #    ax.add_artist(circle)
+    #circle = plt.Circle([x[kk, j] , y[kk, j]], rs, color='r', fill=False) 
+    TT = np.linspace(0, 2 * np.pi, 100)  # Angle parameter
+    #ax.add_artist(circle)
+    for j in range(N):
+        radius = math.sin(math.pi/N)*math.sqrt(800)
+        #print("radius : ", radius)
+    # Parametric equations for a circle in 3D space
+        XX = x[kk, j] + radius * np.cos(TT)
+        YY = y[kk, j] + radius * np.sin(TT)
+        ZZ = np.zeros_like(TT)  # Constant z-coordinate (in this case, at zero)
+
+    # Plot the circle
+
+        ax.plot(XX, YY, ZZ, color='b')
+    
+    
     ax.plot(xb, yb, color='k')
     ax.plot(xX, yY, zZ, 'r', linewidth=1)
-
-    ax.scatter(x[kk, :] , y[kk, :], z[kk,:], c='b', marker='o')
+    for j in range(N):
+            ax.scatter(x[kk, j] , y[kk, j], z[kk,j], c='b', marker='o')
     
     min_distance = minimum_distance_among_robots(x[kk, :] , y[kk, :], z[kk,:])
     print("Minimum distance among all pairs of robots:", min_distance)
